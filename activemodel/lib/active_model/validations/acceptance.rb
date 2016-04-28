335a92efbb579d9626db09da920ab649e63146ba
lvl0nax <lvl0nax@gmail.com>
2016-04-28 09:17:50 +0300

    Little fixes for performance in ActiveModel

--- activemodel/lib/active_model/validations/acceptance.rb
+++ activemodel/lib/active_model/validations/acceptance.rb
@@ -65,9 +65,7 @@ module ActiveModel
 
         def convert_to_reader_name(method_name)
           attr_name = method_name.to_s
-          if attr_name.end_with?("=")
-            attr_name = attr_name[0..-2]
-          end
+          attr_name.chomp!('=')
           attr_name
         end
       end
